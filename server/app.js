322deff7437892ea5ab9396efa1787acde12fc94
Himanshu Singh <hs5317610@gmail.com>
2026-03-10 20:02:04 +0530

    3rd

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 //CORS
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://cineverse-dun.vercel.app"],
     credentials: true
 }))
 
